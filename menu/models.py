2a5b05fd73e4aac155e884ddb0ce4a84f7ba0e5f
DoctorLynch <doctor_linchenko@mail.ru>
2024-02-06 02:31:10 +0300

    it all

--- menu/models.py
+++ menu/models.py
@@ -4,9 +4,10 @@ NULLABLE = {'blank': True, 'null': True}
 
 
 class MenuItem(models.Model):
-    name = models.CharField(max_length=100, verbose_name='наименование')
-    url = models.CharField(max_length=100, verbose_name='ссылка')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, **NULLABLE)
+    name = models.CharField(max_length=255)
+    parent = models.ForeignKey('self', **NULLABLE, related_name='children', on_delete=models.CASCADE)
+    url = models.CharField(max_length=255)
+    named_url = models.CharField(max_length=255, **NULLABLE)
 
     def __str__(self):
         # Строковое отображение объекта
@@ -15,6 +16,5 @@ class MenuItem(models.Model):
     class Meta:
         verbose_name = 'Меню'  # Настройка для наименования одного объекта
         verbose_name_plural = 'Меню'  # Настройка для наименования набора объектов
-        ordering = ('name',)
 
 
--- menu/urls.py
+++ menu/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from menu.apps import MenuConfig
-from menu.views import my_view
+from menu.views import draw_menu
 
 app_name = MenuConfig.name
 
 urlpatterns = [
-    path('', my_view, name='menu')
+    path('<str:menu_name>/', draw_menu, name='draw_menu'),
 ]
